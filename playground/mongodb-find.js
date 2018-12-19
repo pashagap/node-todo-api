654420915b6fc4f3bbd1245649c22f4d7482acf7
pashagap <pasha.gap@gmail.com>
2018-12-19 14:05:48 +0200

    Init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,35 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true }, (err, client) => {
+  if (err) {
+    return console.log('Unable to connect to MonogoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  const db = client.db('TodoApp');
+
+  // db.collection('Todos').find({
+  //   _id: ObjectID("5c1a2dee4bc96bf411ea8c48")
+  // }).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  // db.collection('Todos').find().count().then((count) => {
+  //   console.log(`Todos count: ${count}`);
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  db.collection('Users').find({ age: {$lte: 30} }).toArray().then((users) => {
+    console.log('Users');
+    console.log(JSON.stringify(users, undefined, 2));
+  }, (err) => {
+    console.log('Unable to fetch users', err);
+  });
+
+  // client.close();
+});
